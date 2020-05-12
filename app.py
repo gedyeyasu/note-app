1c07067ba9108b47b2b48b61902e7428df67d6e5
gedyeyasu <gedyeyasu@gmail.com>
2020-05-12 15:54:47 +0300

    finished with bug

---
+++ app.py
@@ -0,0 +1,26 @@
+import datetime
+from flask import Flask, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
+date = datetime.datetime.now()
+
+
+
+
+@app.route("/home", methods=["GET", "POST"])
+def index():
+    if session.get("notes") is None:
+    	session['notes']= []
+
+    if request.method == "POST":
+        note = request.form.get("note")
+        
+        session["notes"].append(note)
+       
+    return render_template("index.html", note= session["notes"], date=date)
